d899c0094c95424b36f2ac6f723f2cea9fa90cd3
yoguze <numano6262@keio.jp>
2025-04-20 01:21:57 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect, flash
+from email.message import EmailMessage
+import smtplib
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+# 管理者のメール設定
+ADMIN_EMAIL = "numanomutsuki@gmail.com"
+EMAIL_PASSWORD = "kexh dbyy bxpi vinl"
+
+@app.route('/', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        try:
+            msg = EmailMessage()
+            msg['Subject'] = f'【お問い合わせ】'
+            msg['From'] = email
+            msg['To'] = ADMIN_EMAIL
+            msg.set_content(f"Name: {name}\nEmail: {email}\n{message}")
+
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                smtp.login(ADMIN_EMAIL, EMAIL_PASSWORD)
+                smtp.send_message(msg)
+
+            flash('送信ありがとうございます！')
+        except Exception as e:
+            flash(f'送信エラー: {e}')
+
+        return redirect('/')
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
