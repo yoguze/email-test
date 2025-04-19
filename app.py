d37e4d18b46d998ab5308976aedb73fb77690279
yoguze <numano6262@keio.jp>
2025-04-20 01:38:34 +0900

    Webサイトを更新

--- app.py
+++ app.py
@@ -36,5 +36,4 @@ def contact():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
